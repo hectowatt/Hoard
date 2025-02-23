75cba61bea52bf00cceb0884cc716b1239923d20
hectowatt <0903tamaki@gmail.com>
2025-02-23 22:27:52 +0900

    DB、Nginx、backendのdocker compose buildとupができるように変更

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const WebSocket = require('ws');
+
+const app = express();
+const port = 3000;
+
+const server = app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  
+  ws.on('message', (message) => {
+    console.log(`Received: ${message}`);
+    ws.send(`Echo: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('WebSocket Server is running');
+});
