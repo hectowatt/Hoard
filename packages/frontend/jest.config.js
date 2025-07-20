d5f15237cb3defa86cf69ef96627527b95cd9a04
hectowatt <w477.dev@gmail.com>
2025-07-20 07:31:49 +0000

    ルートのソースフォルダをpackagesに変更

---
+++ packages/frontend/jest.config.js
@@ -0,0 +1,9 @@
+export default {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["@babel/preset-env", "@babel/preset-react"] }]
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  }
+};
