92acc2b230ceaa85cb0fbf6c2334ae7a9dd82f0c
hectowatt <w477.dev@gmail.com>
2025-12-05 07:35:35 +0000

    nextとreactのバージョン更新で起動しなくなった問題を修正

--- packages/frontend/eslint.config.mjs
+++ packages/frontend/eslint.config.mjs
@@ -1,16 +1,32 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact, { rules } from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import pluginReact from "eslint-plugin-react";
+import { fixupConfigRules } from "@eslint/compat";
 
+export default [
+  js.configs.recommended,
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {rules:{
-    "no-unused-vars": "off",
-  }}
-]);
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    }
+  },
+
+  ...tseslint.configs.recommended,
+
+  ...fixupConfigRules(pluginReact.configs.recommended),
+
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+    }
+  },
+
+];
