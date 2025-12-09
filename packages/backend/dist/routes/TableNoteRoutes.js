0de53d2ec9ba742dde7464e8a3dad913625ddd70
hectowatt <w477.dev@gmail.com>
2025-12-09 02:55:28 +0000

    テーブルノート取得で行番号と列番号でソートするように修正

--- packages/backend/dist/routes/TableNoteRoutes.js
+++ packages/backend/dist/routes/TableNoteRoutes.js
@@ -78,7 +78,7 @@ router.get('/', authMiddleware, async (req, res) => {
             const columnRepository = AppDataSource.getRepository(TableNoteColumn);
             const cellRepository = AppDataSource.getRepository(TableNoteCell);
             const columns = await columnRepository.find({ where: { tableNote: { id: tableNote.id } }, order: { order: 'ASC' } });
-            const rowCells = await cellRepository.find({ where: { tableNote: { id: tableNote.id } }, relations: ['column'], order: { row_index: 'ASC' } });
+            const rowCells = await cellRepository.find({ where: { tableNote: { id: tableNote.id } }, relations: ['column'], order: { row_index: 'ASC', column: { order: 'ASC' } } });
             // rowCellsをrow_indexごとにグループ化して2次元配列に変換
             const groupedRowCells = [];
             rowCells.forEach(cell => {
